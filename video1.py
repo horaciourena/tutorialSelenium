1a545cb5944dc83749cd34d07b8e63e0439d1e4c
luis <horaciourena@hotmail.com>
2020-09-29 12:51:36 -0400

    accesing HTML elements with a website

--- video1.py
+++ video1.py
@@ -1,8 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://youtube.com/horaciourena")
-# print(driver.title) id, class, name, tag
-driver.close()
+driver.get("https://youtube.com/")
+search = driver.find_element_by_name("search_query")
+search.send_keys("Horacio Urena")
+search.send_keys(Keys.RETURN)
+
+try:
+    contenido = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "contents"))
+    )
+    videos = contenido.find_elements_by_tag_name("ytd-video-renderer")
+    for video in videos:
+        titulo = video.find_element_by_id("title-wrapper")
+        print(titulo.text)
+finally:
+    driver.quit()
